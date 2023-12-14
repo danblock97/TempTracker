2358db9374d434fcd0688bd5be1714f6d861ba52
danblock97 <generalgoldiez@gmail.com>
2023-12-14 09:00:30 +0000

    Name Change

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "WeatherPulse",
+	title: "TempTracker",
 	description:
 		"Weather App bootsrapped with NextJS - API Supplied from OpenWeatherMap",
 };
