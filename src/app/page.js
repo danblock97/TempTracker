a41be9f9bcdf14e1936b6af0b26dd6d5a7995435
danblock97 <generalgoldiez@gmail.com>
2023-12-15 20:15:12 +0000

    Code clean up

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ import CurrentWeather from "../components/CurrentWeather";
 import DailyForecast from "../components/DailyForecast";
 import WeeklyForecast from "../components/WeeklyForecast";
 import AirCondition from "@/components/AirCondition";
-import Loading from "react-loading"; // Import the Loading component from react-loading
+import Loading from "react-loading";
 
 export default function Home() {
 	const [city, setCity] = useState("");
-	const [loading, setLoading] = useState(false); // Add a loading state
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const savedCity = sessionStorage.getItem("city");
@@ -20,17 +20,18 @@ export default function Home() {
 	}, []);
 
 	const handleSearch = (location) => {
-		setLoading(true); // Set loading to true when searching
+		setLoading(true);
 		setCity(location);
 		sessionStorage.setItem("city", location);
-		setLoading(false); // Set loading to false when data is loaded
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
 	};
 
 	return (
 		<div className="bg-gray-900 min-h-screen text-white p-4 flex flex-col justify-between">
 			<SearchBar onSearch={handleSearch} />
 			{loading ? (
-				// Display the loading component when loading is true
 				<div className="flex justify-center items-center h-screen">
 					<Loading type="bars" color="#ffffff" height={100} width={100} />
 				</div>
@@ -43,7 +44,6 @@ export default function Home() {
 				)
 			)}
 			{loading ? (
-				// Display the loading component when loading is true
 				<div className="flex justify-center items-center h-screen">
 					<Loading type="bars" color="#ffffff" height={100} width={100} />
 				</div>
